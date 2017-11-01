a2ebba61adf0daa0945ca9d10a3764c315af5424
1245053895 <1245053895@qq.com>
2017-11-01 11:01:29 +0800

    回档

--- src/main/java/com/xh/mapper/RoleMapper.java
+++ src/main/java/com/xh/mapper/RoleMapper.java
@@ -1,8 +1,6 @@
 package com.xh.mapper;
-import com.xh.po.Admin;
 import com.xh.po.AdminRole;
 import com.xh.po.Admingroup;
-import com.xh.po.Admingroupfirstview;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,68 +11,18 @@ import java.util.List;
 @Component
 public interface RoleMapper {
     /**
-     * 查询每个用户对应的角色
-     * @return
+     *  查询出每个后台用户对应的角色名称和角色描述
      */
     public List<AdminRole> queryRoleAndAdmin();
 
     /**
-     * 查询所有管理员列表
-     * @return
+     *  查询出所有角色的角色名称和角色描述
      */
-    public List<Admin> selectAllAdmin();
+    public List<Admingroup>queryAllRole();
 
     /**
-     * 从某个位置查询一定数量管理员列表
-     * @param start  从何处开始
-     * @param count  总共查多少
-     * @return
-     */
-    public List<Admin> selectAdmin(Integer start,Integer count);
-    /**
-     * 查询所有角色列表
-     * @return
-     */
-    public List<Admingroup> selectAllAdmingroup();
-
-    /**
-     * 从某个位置查询一定数量角色列表
-     * @param start
-     * @param count
-     * @return
-     */
-    public List<Admingroup> selectAdmingroup(Integer start,Integer count);
-
-    /**
-     * 添加角色及设置角色应有的权限
-     * @param admingroup
-     */
-    public void insertRole(Admingroup admingroup, List<Admingroupfirstview> Admingroupfirstviews);
-
-    /**
-     * 通过id删除角色
+     * 实现对角色修改页面根据角色的id删除角色
      * @param id
      */
-    public void deleteRoleById(Integer id);
-
-    /**
-     * 通过id批量删除
-     * @param ids
-     */
-    public void deleteRolesByIds(Integer[] ids);
-
-    /**
-     *通过管理员id检查输入的密码,Admin为空则密码不正确
-     * @return
-     */
-    public Admin  queryPasswordById(Integer id,String password);
-
-    /**
-     * 通过管理员id修改管理员密码(修改前先调用queryPasswordById 验证原密码是否正确)
-     * @param id
-     * @param newPassword
-     */
-    public void updataPasswordById(Integer id,String newPassword);
-
-
+    public  void deleteRoleById(int id);
 }
--- src/main/java/com/xh/serviceimp/RoleManageServImp.java
+++ src/main/java/com/xh/serviceimp/RoleManageServImp.java
@@ -2,8 +2,10 @@ package com.xh.serviceimp;
 
 import com.xh.mapper.RoleMapper;
 import com.xh.po.AdminRole;
+import com.xh.po.Admingroup;
 import com.xh.service.RoleManageServ;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
  */
 
 
+@Component
 public class RoleManageServImp implements RoleManageServ {
     @Autowired
     private RoleMapper roleMapper;
@@ -20,5 +23,15 @@ public class RoleManageServImp implements RoleManageServ {
         List<AdminRole> adminRoles=roleMapper.queryRoleAndAdmin();
         return adminRoles;
     }
+
+    public List<Admingroup> queryAllRole() {
+        List<Admingroup> admingroups= roleMapper.queryAllRole();
+        return admingroups;
+    }
+
+    public void deleteRoleById(int id) {
+        roleMapper.deleteRoleById(id);
+    }
+
 
 }

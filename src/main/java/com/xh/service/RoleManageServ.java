aca8edb04fa29d39e82ee80f90f9765bae2d0cac
Mycheny <1208640961@qq.com>
2017-11-01 10:21:44 +0800

    wcy update

--- src/main/java/com/xh/service/RoleManageServ.java
+++ src/main/java/com/xh/service/RoleManageServ.java
@@ -2,8 +2,6 @@ package com.xh.service;
 
 
 import com.xh.po.AdminRole;
-import com.xh.po.Admingroup;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,8 +9,7 @@ import java.util.List;
 /**
  * Created by KAIRUN on 2017/10/30.
  */
+
 public interface RoleManageServ {
     public List<AdminRole> queryRoleAndAdmin();
-    public List<Admingroup>queryAllRole();
-    public  void deleteRoleById(int id);
 }
